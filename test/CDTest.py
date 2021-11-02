a850b6ec353dc6d3afd20b0c132123974f75e2c9
sartain <alex.sartain@zuhlke.com>
2021-11-02 12:20:18 +0000

    Add charts to pass all tests

--- test/CDTest.py
+++ test/CDTest.py
@@ -7,35 +7,40 @@ from CreditCardProvider import CreditCardProvider
 
 
 class CDTest(unittest.TestCase):
-    """
+
     def test_buy_cd_when_payment_accepted_and_in_stock(self):
+        charts = Charts(10)
         cd = CD("artist_c", "title_d", 1, 9.99)
         credit_card = CreditCardProvider(True)
-        self.assertEqual(True, cd.buy_cd(credit_card, 1))
+        self.assertEqual(True, cd.buy_cd(credit_card, 1, charts))
 
     def test_buy_cd_when_payment_not_accepted_and_in_stock(self):
+        charts = Charts(10)
         cd = CD("artist_c", "title_d", 1, 9.99)
         credit_card = CreditCardProvider(False)
-        self.assertEqual(False, cd.buy_cd(credit_card, 1))
+        self.assertEqual(False, cd.buy_cd(credit_card, 1, charts))
 
     def test_buy_cd_when_not_in_stock_payment_accepted(self):
+        charts = Charts(10)
         cd = CD("artist_c", "title_d", 0, 9.99)
         credit_card = CreditCardProvider(True)
-        self.assertEqual(False, cd.buy_cd(credit_card, 1))
+        self.assertEqual(False, cd.buy_cd(credit_card, 1, charts))
 
     def test_buy_one_cd_remove_one_cd_stock_from_warehouse_out_of_stock(self):
+        charts = Charts(10)
         cd = CD("artist_c", "title_d", 1, 9.99)
         credit_card = CreditCardProvider(True)
-        cd.buy_cd(credit_card, 1) #Buy cd to reduce stock level
+        cd.buy_cd(credit_card, 1, charts) #Buy cd to reduce stock level
         #cd out of stock cannot buy again
-        self.assertEqual(False, cd.buy_cd(credit_card, 1))
+        self.assertEqual(False, cd.buy_cd(credit_card, 1, charts))
 
     def test_buy_two_cd_in_a_row_in_stock_payment_accepted(self):
+        charts = Charts(10)
         cd = CD("artist_c", "title_d", 2, 9.99)
         credit_card = CreditCardProvider(True)
-        cd.buy_cd(credit_card, 1) #Buy cd to reduce stock level
-        self.assertEqual(True, cd.buy_cd(credit_card, 1))
-"""
+        cd.buy_cd(credit_card, 1, charts) #Buy cd to reduce stock level
+        self.assertEqual(True, cd.buy_cd(credit_card, 1, charts))
+
     def test_charts_alerted_when_sale_made(self):
         charts = Charts(10)
         charts.sale_update = MagicMock()
