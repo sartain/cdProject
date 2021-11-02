e0b80389c70f979f240967ebc6a71ac4dae25dad
sartain <alex.sartain@zuhlke.com>
2021-11-02 11:51:25 +0000

    Add price and quantity to cd and buying cd

--- CD.py
+++ CD.py
@@ -1,13 +1,14 @@
 class CD:
-    def __init__(self, artist, title, stock):
+    def __init__(self, artist, title, stock, price):
         self.artist = artist
         self.title = title
         self.stock = stock
+        self.price = price
 
-    def buy_cd(self, credit_card):
+    def buy_cd(self, credit_card, quantity):
         if credit_card.payment_accepted:
-            if self.stock > 0:
-                self.stock -= 1
+            if self.stock - quantity >= 0:
+                self.stock -= quantity
                 return True
             return False #not in stock
         return False #payment not accepted
--- test/CDTest.py
+++ test/CDTest.py
@@ -9,46 +9,32 @@ from Warehouse import Warehouse
 class CDTest(unittest.TestCase):
 
     def test_buy_cd_when_payment_accepted_and_in_stock(self):
-        cd = CD("artist_c", "title_d", 1)
+        cd = CD("artist_c", "title_d", 1, 9.99)
         credit_card = CreditCardProvider(True)
-        self.assertEqual(True, cd.buy_cd(credit_card))
+        self.assertEqual(True, cd.buy_cd(credit_card, 1))
 
     def test_buy_cd_when_payment_not_accepted_and_in_stock(self):
-        cd = CD("artist_c", "title_d", 1)
+        cd = CD("artist_c", "title_d", 1, 9.99)
         credit_card = CreditCardProvider(False)
-        self.assertEqual(False, cd.buy_cd(credit_card))
+        self.assertEqual(False, cd.buy_cd(credit_card, 1))
 
     def test_buy_cd_when_not_in_stock_payment_accepted(self):
-        cd = CD("artist_c", "title_d", 0)
+        cd = CD("artist_c", "title_d", 0, 9.99)
         credit_card = CreditCardProvider(True)
-        self.assertEqual(False, cd.buy_cd(credit_card))
+        self.assertEqual(False, cd.buy_cd(credit_card, 1))
 
     def test_buy_one_cd_remove_one_cd_stock_from_warehouse_out_of_stock(self):
-        cd = CD("artist_c", "title_d", 1)
+        cd = CD("artist_c", "title_d", 1, 9.99)
         credit_card = CreditCardProvider(True)
-        cd.buy_cd(credit_card) #Buy cd to reduce stock level
+        cd.buy_cd(credit_card, 1) #Buy cd to reduce stock level
         #cd out of stock cannot buy again
-        self.assertEqual(False, cd.buy_cd(credit_card))
+        self.assertEqual(False, cd.buy_cd(credit_card, 1))
 
     def test_buy_two_cd_in_a_row_in_stock_payment_accepted(self):
-        cd = CD("artist_c", "title_d", 2)
+        cd = CD("artist_c", "title_d", 2, 9.99)
         credit_card = CreditCardProvider(True)
-        cd.buy_cd(credit_card) #Buy cd to reduce stock level
-        #cd out of stock cannot buy again
-        self.assertEqual(True, cd.buy_cd(credit_card))
-
-
-    # def test_get_cd_from_artist_and_title_when_not_exist(self):
-    #    warehouse = Warehouse()
-    #    self.assertEqual(False, warehouse.findCD("artist_a", "title_b"))
-
-    # def test_get_cd_when_exist(self):
-    #    warehouse = Warehouse()
-    #    self.assertEqual(True, warehouse.findCD("artist_c", "title_d"))
-
-    # def test_get_cd_object_when_exist(self):
-    #    warehouse = Warehouse()
-    #    self.assertEqual(CD("artist_e", "title_f"), warehouse.findCD("artist_e", "title_f"))
+        cd.buy_cd(credit_card, 1) #Buy cd to reduce stock level
+        self.assertEqual(True, cd.buy_cd(credit_card, 1))
 
 
 if __name__ == '__main__':
