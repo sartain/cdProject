5fd98369e9899e88c890c679e0eda9953e7774c7
sartain <alex.sartain@zuhlke.com>
2021-11-02 12:52:12 +0000

    Refactor price if in top 100

--- CD.py
+++ CD.py
@@ -8,15 +8,10 @@ class CD:
     def buy_cd(self, credit_card, quantity, charts, competitorPrice):
         chart_position = charts.current_chart_position(self.artist, self.title)
         competitor_price = competitorPrice.get_price()
+        price_to_pay = self.price
         if charts.in_top_100():
-            if credit_card.can_afford(competitor_price - 1.00):
-                if self.stock - quantity >= 0:
-                    self.stock -= quantity
-                    charts.sale_update(self.artist, self.title, quantity)
-                    return True
-                return False  # not in stock
-            return False  # payment not accepted
-        if credit_card.can_afford(self.price):
+            price_to_pay = competitor_price - 1.00
+        if credit_card.can_afford(price_to_pay):
             if self.stock - quantity >= 0:
                 self.stock -= quantity
                 charts.sale_update(self.artist, self.title, quantity)
